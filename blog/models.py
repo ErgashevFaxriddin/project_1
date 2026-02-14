7d2bd01ae45e8354e6969bff7ff7443b4ddea890
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-14 23:06:02 +0500

    added book and genres

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,30 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True, null=True)
     age = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name.title() #admin panelda foydalanuvchi ismi bilan korinish uchun
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name.title() #admin panelda foydalanuvchi ismi bilan korinish uchun
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100) #ism kiritish majburiy
+    author = models.ForeignKey(
+        Author,     #Author modeliga ForeignKey
+        null=True,  #DBda NULL bo‘lishi mumkin
+        on_delete=models.SET_NULL #agar author o'chirilsa, author maydoni NULL bo'ladi on_delete=models.CASCADE #agar author o'chib ketsa book ha o'chib ketadi
+        )
+    genre = models.ForeignKey(Genre,
+        null=True,
+        on_delete=models.SET_NULL
+        )
+
+    def __str__(self):
+        return self.name.title() #admin panelda foydalanuvchi ismi bilan korinish uchun
+
